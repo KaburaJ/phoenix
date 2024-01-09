6302077a2ca98545971d4e1d5452b22e25eb0683
Joan Kabura <kaburajoan25@gmail.com>
2024-01-09 19:16:43 +0300

    Carousel sorted times3

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "~slick-carousel/slick/slick.css"; 
+import "~slick-carousel/slick/slick-theme.css";
 import { FaBeer, FaChartBar, FaCloud, FaDoorClosed, FaHammer, FaLaptop, FaRobot, FaServer, FaShieldAlt } from "react-icons/fa";
 import Navbar from '../../common/Navbar/Navbar'
 import Footer from '../../common/footer/Footer'
